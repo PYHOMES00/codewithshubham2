337db223f4d9c6350220f71c22a2bd5fa960e06c
nothing... <161198246+PYHOMES00@users.noreply.github.com>
2024-07-16 20:01:46 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -58,3 +58,12 @@ ID - <code>{}</code>
 
 Nᴀᴍᴇ - {}</b>
 """
+
+
+    RESTART_TXT = """
+<b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
+
+📅 Dᴀᴛᴇ : <code>{}</code>
+⏰ Tɪᴍᴇ : <code>{}</code>
+🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>Asia/Kolkata</code>
+🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code>v2.7.1 [ Sᴛᴀʙʟᴇ ]</code></b>"""
